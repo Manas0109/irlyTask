cc98ce79d710b2ffbca8fb4bbf0a0aae799bb6e4
Manas0109 <manasgpta@gmail.com>
2024-01-04 00:53:38 +0530

    Initial Commit

---
+++ irlyTask/irlyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  irlyTaskApp.swift
+//  irlyTask
+//
+//  Created by metal_fury on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct irlyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
